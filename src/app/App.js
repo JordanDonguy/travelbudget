68b8d9421f399c7c7fb4111b221f619efe3a8865
Jordan <jordan.donguy@gmail.com>
2025-01-29 21:43:54 +0100

    First commit

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  Route,
+  BrowserRouter,
+  Routes
+} from "react-router-dom";
+
+export default function App () {
+  return (
+    <BrowserRouter>
+
+    </BrowserRouter>
+  )
+};
